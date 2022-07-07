d0157d08b4109d38bd0e5247de9636403a7096f6
William Furtado <willfurtado@berkeley.edu>
2022-07-07 15:30:24 -0400

    Adding hw01 dist to public repo

---
+++ hw/hw01/student/tests/q6.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q6',
+    'points': 2,
+    'suites': [   {   'cases': [{'code': '>>> isinstance(sleeper_count, int)\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> sleeper_count == 98907\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
