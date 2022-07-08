031d49764376ad7429ea7a8523b55e5aae2266e1
James Weichert <james.p.weichert@gmail.com>
2022-07-07 20:22:04 -0700

    Trying to fix merge conflict

---
+++ new_homework/homework01/tests/q7b.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q7b',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(indiv_bmi, float)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(indiv_bmi, 21.872346022399725)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ new_homework/homework01/tests/q7c.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q7c',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(bmi_array, np.ndarray)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(bmi_array.item(54), 37.15099806391631)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(np.mean(bmi_array), 28.87746038703732)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
