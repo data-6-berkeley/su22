f7426ea45d9eefc14c4e9432e8a267bda1a0618f
James Weichert <james.p.weichert@gmail.com>
2022-07-07 20:12:57 -0700

    Trying to fix merge conflict issue

---
+++ homework/hw01/tests/q5b.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q5b',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(percent_personal_doc, float)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(percent_personal_doc, 0.83341)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ homework/hw01/tests/q8.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q8',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(avg_sleep, float)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(avg_sleep, 6.967529609627749)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
