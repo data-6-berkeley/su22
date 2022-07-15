e0dd009c4742245afa12cd48f9eb706d01a70976
William Furtado <willfurtado@berkeley.edu>
2022-07-14 20:51:16 -0400

    Adding hw02 to public repo

---
+++ hw/hw02/tests/q1a.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q1a',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> # Make sure that you assign `sfo` to a table\n>>> isinstance(sfo, Table)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> sfo.num_rows == 22869\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> sfo.num_columns == 7\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw02/tests/q4bi.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q4bi',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(q4bi_answer, str)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> q4bi_answer in ("Adelie", "Chinstrap", "Gentoo")\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> q4bi_answer == "Gentoo"\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
