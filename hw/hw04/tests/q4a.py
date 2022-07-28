f20711ef369b854671f20692df8d2bb6944535ed
William Furtado <willfurtado@berkeley.edu>
2022-07-28 18:53:00 -0400

    Composing hw04 Student Facing

--- hw/hw04/tests/q4a.py
+++ hw/hw04/tests/q4a.py
@@ -1,4 +1,4 @@
-test = {   'name': 'q4',
+test = {   'name': 'q4a',
     'points': 3,
     'suites': [   {   'cases': [   {'code': '>>> callable(one_streak)\nTrue', 'hidden': False, 'locked': False},
                                    {'code': '>>> one_streak(make_array(13, 3, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 2, 5, 7, 8)) == 9\nTrue', 'hidden': False, 'locked': False},
