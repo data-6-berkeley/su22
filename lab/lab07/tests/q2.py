0a0455f8a36148009424d92db80917d0ff62f5a1
William Furtado <willfurtado@berkeley.edu>
2022-07-27 20:24:30 -0400

    Composing lab07 Student Facing

---
+++ lab/lab07/tests/q2.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2',
+    'points': 0,
+    'suites': [   {   'cases': [   {'code': ">>> all(starts_with_vowels(some_words) == make_array('alligator', 'aardvark', 'owl', 'unicorn'))\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> all(starts_with_vowels(make_array("bear")) == make_array())\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> "zebra" not in starts_with_vowels(some_words)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
