8896c7bf4ad83876f49e08bd92d9d901f8c361d3
William Furtado <willfurtado@berkeley.edu>
2022-07-27 23:05:25 -0400

    Composing hw04 Student Facing

---
+++ hw/hw04/tests/q4.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q4',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> callable(one_streak)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> one_streak(make_array(13, 3, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 2, 5, 7, 8)) == 9\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> one_streak(make_array(4, 1, 1, 1, 2, 3, 11, 1, 1, 1, 1, 1)) == 5\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> one_streak(make_array(5, 4, 1, 1, 1, 3, 2, 1, 2, 3)) == 3\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> one_streak(make_array(1, 2, 3, 4, 5)) == 1\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw04/tests/q5a.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q5a',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> callable(single_number)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': ">>> single_number(30) == 'fizz buzz'\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> single_number(9) == 'fizz'\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> single_number(10) == 'buzz'\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> # Make sure you're returning a string, not an int!\n>>> single_number(1) == '1'\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
